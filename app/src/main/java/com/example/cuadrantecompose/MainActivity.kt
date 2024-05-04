ba3e2a2e4ebca30ad48f759e8bd61e9c827ba5b6
Sauby21 <146679738+Sauby21@users.noreply.github.com>
2024-05-03 23:44:46 -0500

    n

--- app/src/main/java/com/example/cuadrantecompose/MainActivity.kt
+++ app/src/main/java/com/example/cuadrantecompose/MainActivity.kt
@@ -5,11 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -70,7 +68,7 @@ fun Quadrant(title: String, text: String, modifier: Modifier = Modifier) {
 }
 @Composable
 fun Todo(title: String, text: String) {
-    Column (){
+    Column{
         Row (Modifier.weight(1f)){
             Quadrant(
                 title = title,
